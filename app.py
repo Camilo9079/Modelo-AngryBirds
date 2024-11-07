5b1ca2a6aeee0cbe736d7a36ddfd745bb803a518
Juan Camilo Reyes Gonzalez <106204611+Camilo9079@users.noreply.github.com>
2024-11-06 19:46:51 -0500

    Reset appy

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, request, jsonify , send_from_directory
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import os
-from keras.models import load_model
-import os
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.applications.imagenet_utils import preprocess_input
+from io import BytesIO
+from PIL import Image  # Use PIL for image processing in memory
 
-
+# Class names list
 names = ['Anambé Barrado',
 'Anambé Cinéreo',
 'Anambé Unicolor',
@@ -63,81 +62,67 @@ names = ['Anambé Barrado',
 'Titirijí Perlado',
 'Yacutoro']
 
-# Cargar el modelo
-dirname = os.path.dirname(__file__)
-modelt = load_model(os.path.join(dirname, 'final_model.keras'))
-#modelt = custom_vgg_model
-
+# Initialize the app
 app = Flask(__name__)
+app.config['DEBUG'] = os.environ.get('FLASK_DEBUG')
+
+# Allow CORS for all origins
 CORS(app)
 
-# Set the folder where you want to save the uploaded images
-UPLOAD_FOLDER = './uploaded_images'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# Load the model
+model_path = os.path.join(os.path.dirname(__file__), 'final_model.keras')
+modelt = load_model(model_path)
 
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-@app.route('/api/', methods=['GET'])
-def get_example():
-    data = {"message": "Este es un ejemplo de respuesta GET"}
-    return jsonify(data)
-
-@app.route('/', methods=['POST'])
-def upload_image():
-    # Check if the POST request contains the 'file' part
-    if 'file' not in request.files:
-        return jsonify({"error": "No file part in the request"}), 400
-    
-    file = request.files['file']
-
-    # If no file was selected for uploading
-    if file.filename == '':
-        return jsonify({"error": "No selected file"}), 400
-    
-    # Check if the file is an allowed image type (optional)
-    if file and allowed_file(file.filename):
-        # Save the file
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(filepath)
-        # Ruta de la imagen de prueba
-        # Leer la imagen, cambiar tamaño y preprocesar
-        imaget=cv2.resize(cv2.imread(filepath), (224, 224), interpolation = cv2.INTER_AREA)
-        xt = np.asarray(imaget)
-        xt=preprocess_input(xt)
-        xt = np.expand_dims(xt,axis=0)
-        # Obtener las predicciones del modelo
-        preds = modelt.predict(xt)
-
-        # Obtener la clase predicha y su porcentaje de confianza
-        predicted_class_index = np.argmax(preds)
-        predicted_class_name = names[predicted_class_index]
-        confidence_percentage = preds[0][predicted_class_index] * 100
-
-        # Imprimir el resultado
-        print(f'Clase predicha: {predicted_class_name}, Porcentaje de confianza: {confidence_percentage:.2f}%')
-
-        # Mostrar la imagen
-        #plt.imshow(cv2.cvtColor(np.asarray(imaget), cv2.COLOR_BGR2RGB))
-        #plt.axis('off')
-        #plt.show()
-        return jsonify({"message": f'Clase predicha: {predicted_class_name}, Porcentaje de confianza: {confidence_percentage:.2f}%', "file_path": filepath}), 200
-    else:
-        return jsonify({"error": "Invalid file type"}), 400
-    
-    
+# Check if file type is allowed
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# Endpoint to classify image
+@app.route('/classify', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part in the request"}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+    if file and allowed_file(file.filename):
+        try:
+            # Open the image directly from memory
+            image = Image.open(BytesIO(file.read()))
+            image = image.resize((224, 224))  # Resize to model input size
+            image_array = np.expand_dims(preprocess_input(np.array(image)), axis=0)
+
+            # Get predictions
+            preds = modelt.predict(image_array)
+            predicted_class_index = np.argmax(preds)
+            if not (predicted_class_index < len(names)):
+                return jsonify({"error": "Predicted index is out of range."}), 500
+            predicted_class_name = names[predicted_class_index]
+            confidence_percentage = preds[0][predicted_class_index] * 100
+
+            return jsonify({
+                "message": f'Predicted class: {predicted_class_name}, Confidence: {confidence_percentage:.2f}%',
+            }), 200
+
+        except Exception as e:
+            return jsonify({"error": f"Error processing the image: {str(e)}"}), 500
+
+    return jsonify({"error": "Invalid file type"}), 400
+
+# Custom 404 error handler
 @app.errorhandler(404)
 def not_found(error):
-    return jsonify({"error": "Recurso no encontrado"}), 404
+    return jsonify({"error": "Resource not found"}), 404
 
+# Serve interface
 @app.route('/')
 def serve_interface():
     return send_from_directory('.', 'index.html')
 
-# Ejecutamos la app
+# Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080)) 
+    app.run(host='0.0.0.0', port=port)
